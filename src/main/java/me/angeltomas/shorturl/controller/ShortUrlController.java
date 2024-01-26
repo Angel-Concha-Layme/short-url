06d64f3e15fe371780bb09d22fb9b640ba183cf1
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-26 10:41:19 -0500

    Remove unused imports in ShortUrlController and UrlService

--- src/main/java/me/angeltomas/shorturl/controller/ShortUrlController.java
+++ src/main/java/me/angeltomas/shorturl/controller/ShortUrlController.java
@@ -1,16 +1,11 @@
 package me.angeltomas.shorturl.controller;
 
 import lombok.AllArgsConstructor;
-import me.angeltomas.shorturl.entity.Url;
-import me.angeltomas.shorturl.service.UrlService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/shorturl")
 @AllArgsConstructor
 public class ShortUrlController {
-
-    private UrlService urlService;
 
 }
