e9b8a5d120c90786d52e41258cb6573668f64d84
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-26 10:36:55 -0500

    Add initial database migration scripts

---
+++ src/main/resources/db/migration/V1__Initial_migration.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `password` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `url` (
+  `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+  `complete_url` VARCHAR(2048) NOT NULL,
+  `short_url` VARCHAR(7) NOT NULL UNIQUE,
+  `name` VARCHAR(255),
+  `description` TEXT,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME,
+  `user_id` INT,
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `quick_url` (
+  `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+  `complete_url` VARCHAR(2048) NOT NULL,
+  `short_url` VARCHAR(7) NOT NULL UNIQUE
+);
