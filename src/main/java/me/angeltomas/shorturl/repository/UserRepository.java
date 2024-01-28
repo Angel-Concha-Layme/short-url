caa852bdc0479c087c0a7431928fe10ac0d94f3a
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-28 12:51:11 -0500

    Update UserRepository imports and add new methods

--- src/main/java/me/angeltomas/shorturl/repository/UserRepository.java
+++ src/main/java/me/angeltomas/shorturl/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package me.angeltomas.shorturl.repository;
 
-import me.angeltomas.shorturl.entity.User;
+import me.angeltomas.shorturl.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
 }
