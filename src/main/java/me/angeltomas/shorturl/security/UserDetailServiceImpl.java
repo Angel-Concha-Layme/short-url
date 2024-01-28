4ac62e399956cdf00613d5dce5de173c998d1e53
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-28 12:51:23 -0500

    Refactor UserDetailServiceImpl and UserDetailsImpl classes

--- src/main/java/me/angeltomas/shorturl/security/UserDetailServiceImpl.java
+++ src/main/java/me/angeltomas/shorturl/security/UserDetailServiceImpl.java
@@ -1,2 +1,24 @@
-package me.angeltomas.shorturl.security;public class UserDetailServiceImpl {
+package me.angeltomas.shorturl.security;
+
+
+import lombok.AllArgsConstructor;
+import me.angeltomas.shorturl.entity.user.User;
+import me.angeltomas.shorturl.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService{
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found"));
+
+        return new UserDetailsImpl(user);
+    }
 }
--- src/main/java/me/angeltomas/shorturl/security/UserDetailsImpl.java
+++ src/main/java/me/angeltomas/shorturl/security/UserDetailsImpl.java
@@ -1,2 +1,54 @@
-package me.angeltomas.shorturl.security;public class UserDetailsImpl {
+package me.angeltomas.shorturl.security;
+
+
+import lombok.AllArgsConstructor;
+import me.angeltomas.shorturl.entity.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@AllArgsConstructor
+public class UserDetailsImpl  implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    public String getFullName() {
+        return user.getName();
+    }
 }
