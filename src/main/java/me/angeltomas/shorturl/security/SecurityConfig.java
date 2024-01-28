10dbecf3662b7af18d14e0b29c7f0f7f70232362
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-28 12:51:36 -0500

    Refactor JWTAuthorizationFilter and SecurityConfig classes

--- src/main/java/me/angeltomas/shorturl/security/SecurityConfig.java
+++ src/main/java/me/angeltomas/shorturl/security/SecurityConfig.java
@@ -1,2 +1,16 @@
-package me.angeltomas.shorturl.security;public class SecurityConfig {
+package me.angeltomas.shorturl.security;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;
 }
