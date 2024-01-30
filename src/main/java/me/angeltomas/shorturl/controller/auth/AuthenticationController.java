fc31d013a40ddd1f4fd2de4e8fc9c1501ac980d3
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-30 01:43:25 -0500

    Fix typo in AuthenticationController class name

--- src/main/java/me/angeltomas/shorturl/controller/auth/AuthenticationController.java
+++ src/main/java/me/angeltomas/shorturl/controller/auth/AuthenticationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class AuthenticacionController {
+public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
