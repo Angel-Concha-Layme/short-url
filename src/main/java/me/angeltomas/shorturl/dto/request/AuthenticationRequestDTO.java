9e4c85252df65e27ee4f7e641540f34e7ddfddc7
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-30 01:45:12 -0500

    Add fields to AuthenticationRequestDTO and RegisterRequestDTO

--- src/main/java/me/angeltomas/shorturl/dto/request/AuthenticationRequestDTO.java
+++ src/main/java/me/angeltomas/shorturl/dto/request/AuthenticationRequestDTO.java
@@ -1,2 +1,7 @@
-package me.angeltomas.shorturl.dto.request;public record AuthenticationRequestDTO() {
+package me.angeltomas.shorturl.dto.request;
+
+public record AuthenticationRequestDTO(
+        String email,
+        String password
+) {
 }
--- src/main/java/me/angeltomas/shorturl/dto/request/RegisterRequestDTO.java
+++ src/main/java/me/angeltomas/shorturl/dto/request/RegisterRequestDTO.java
@@ -1,2 +1,8 @@
-package me.angeltomas.shorturl.dto.request;public record RegisterRequestDTO() {
+package me.angeltomas.shorturl.dto.request;
+
+public record RegisterRequestDTO(
+        String name,
+        String email,
+        String password
+) {
 }
