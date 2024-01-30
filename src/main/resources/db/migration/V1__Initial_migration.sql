bd1c580e65ee7e5bf2914dfc1337fd52aae4415c
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-30 01:43:49 -0500

    Refactor table names and add role column

--- src/main/resources/db/migration/V1__Initial_migration.sql
+++ src/main/resources/db/migration/V1__Initial_migration.sql
@@ -1,11 +1,12 @@
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL UNIQUE,
-  `password` VARCHAR(255) NOT NULL
+  `password` VARCHAR(255) NOT NULL,
+  `role` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `url` (
+CREATE TABLE IF NOT EXISTS `urls` (
   `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `complete_url` VARCHAR(2048) NOT NULL,
   `short_url` VARCHAR(7) NOT NULL UNIQUE,
@@ -14,10 +15,10 @@ CREATE TABLE IF NOT EXISTS `url` (
   `created_at` DATETIME NOT NULL,
   `updated_at` DATETIME,
   `user_id` INT,
-  FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `quick_url` (
+CREATE TABLE IF NOT EXISTS `quick_urls` (
   `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `complete_url` VARCHAR(2048) NOT NULL,
   `short_url` VARCHAR(7) NOT NULL UNIQUE
