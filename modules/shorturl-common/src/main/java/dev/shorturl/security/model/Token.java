89524df3e526bbe92d50515fa3947f87ac4de908
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-01-30 15:59:47 -0500

    feat(security): add Token entity for JWT management

---
+++ modules/shorturl-common/src/main/java/dev/shorturl/security/model/Token.java
@@ -0,0 +1,33 @@
+package dev.shorturl.security.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token")
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType = TokenType.BEARER;
+
+    private boolean revoked;
+
+    private boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
