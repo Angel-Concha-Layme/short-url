a3f2f743d62368dcb61c95e45d1ca377f5a20aa4
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-01-30 16:12:48 -0500

    feat(model): add GuestLink entity

---
+++ modules/shorturl-api/src/main/java/dev/shorturl/model/GuestLink.java
@@ -0,0 +1,23 @@
+package dev.shorturl.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "guest_links")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GuestLink {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String url;
+  private String slug;
+  private LocalDateTime createdAt;
+  private LocalDateTime expiresAt;
+
+}
