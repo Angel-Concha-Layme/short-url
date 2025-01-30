1fb1822346825b948f37b222d6cdc13379730a57
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-01-30 16:09:39 -0500

    feat(auth): add `AuthController` for user registration and authentication

---
+++ modules/shorturl-common/src/main/java/dev/shorturl/security/controller/AuthController.java
@@ -0,0 +1,34 @@
+package dev.shorturl.security.controller;
+
+import dev.shorturl.security.dto.AuthenticationRequestDTO;
+import dev.shorturl.security.dto.AuthenticationResponseDTO;
+import dev.shorturl.security.dto.RegisterRequestDTO;
+import dev.shorturl.security.service.AuthService;
+import jakarta.annotation.security.PermitAll;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+  private final AuthService authservice;
+
+  public AuthController(AuthService authservice) {
+    this.authservice = authservice;
+  }
+
+  @PostMapping("/register")
+  @PermitAll
+  public ResponseEntity<AuthenticationResponseDTO> register(@RequestBody RegisterRequestDTO registerRequestDTO) {
+    return ResponseEntity.ok(this.authservice.register(registerRequestDTO));
+  }
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<AuthenticationResponseDTO> authenticate(@RequestBody AuthenticationRequestDTO authenticationRequestDTO) {
+    return ResponseEntity.ok(this.authservice.authenticate(authenticationRequestDTO));
+  }
+
+}
