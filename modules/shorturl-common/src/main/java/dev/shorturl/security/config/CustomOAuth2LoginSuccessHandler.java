2c0576e770a0faa920b660977441043050c23828
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-02-06 15:36:43 -0500

    refactor(auth): update AuthService and OAuth2LoginSuccessHandler to use TokenService
    - Replace duplicated token management code in AuthService and CustomOAuth2LoginSuccessHandler with calls to TokenService.
    - Eliminates circular dependency issues and improves maintainability.

--- modules/shorturl-common/src/main/java/dev/shorturl/security/config/CustomOAuth2LoginSuccessHandler.java
+++ modules/shorturl-common/src/main/java/dev/shorturl/security/config/CustomOAuth2LoginSuccessHandler.java
@@ -3,12 +3,10 @@ package dev.shorturl.security.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.shorturl.security.dto.AuthenticationResponseDTO;
 import dev.shorturl.security.model.Role;
-import dev.shorturl.security.model.Token;
-import dev.shorturl.security.model.TokenType;
 import dev.shorturl.security.model.User;
-import dev.shorturl.security.repository.TokenRepository;
 import dev.shorturl.security.repository.UserRepository;
 import dev.shorturl.security.service.JWTService;
+import dev.shorturl.security.service.TokenService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
@@ -23,77 +21,56 @@ import java.util.UUID;
 @Component
 public class CustomOAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
-  private final JWTService jwtService;
-  private final UserRepository userRepository;
-  private final PasswordEncoder passwordEncoder;
-  private final TokenRepository tokenRepository;
-  private final ObjectMapper jacksonObjectMapper;
+    private final JWTService jwtService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+    private final ObjectMapper jacksonObjectMapper;
 
-  public CustomOAuth2LoginSuccessHandler(JWTService jwtService, UserRepository userRepository, PasswordEncoder passwordEncoder, TokenRepository tokenRepository, ObjectMapper jacksonObjectMapper) {
-    this.jwtService = jwtService;
-    this.userRepository = userRepository;
-    this.passwordEncoder = passwordEncoder;
-    this.tokenRepository = tokenRepository;
-    this.jacksonObjectMapper = jacksonObjectMapper;
-  }
+    public CustomOAuth2LoginSuccessHandler(JWTService jwtService, UserRepository userRepository,
+                                             PasswordEncoder passwordEncoder, TokenService tokenService,
+                                             ObjectMapper jacksonObjectMapper) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenService = tokenService;
+        this.jacksonObjectMapper = jacksonObjectMapper;
+    }
 
-  @Override
-  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-      throws IOException {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException {
 
-    OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-    System.out.println("OAuth2 Attributes: " + oAuth2User.getAttributes());
-    String email = (String) oAuth2User.getAttributes().get("email");
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("OAuth2 Attributes: " + oAuth2User.getAttributes());
+        String email = (String) oAuth2User.getAttributes().get("email");
 
+        User user = userRepository.findByEmail(email).orElseGet(() -> {
+            String firstName = (String) oAuth2User.getAttributes().getOrDefault("given_name", "Username");
+            String lastName = (String) oAuth2User.getAttributes().getOrDefault("family_name", "Default");
+            String defaultPassword = UUID.randomUUID().toString();
+            Role role = Role.USER;
 
-    User user = userRepository.findByEmail(email).orElseGet(() -> {
-      String firstName = (String) oAuth2User.getAttributes().getOrDefault("given_name", "Username");
-      String lastName = (String) oAuth2User.getAttributes().getOrDefault("family_name", "Default");
-      String defaultPassword = UUID.randomUUID().toString();
-      Role role = Role.USER;
+            User newUser = new User();
+            newUser.setFirstName(firstName);
+            newUser.setLastName(lastName);
+            newUser.setEmail(email);
+            newUser.setPassword(passwordEncoder.encode(defaultPassword));
+            newUser.setRole(role);
 
-      User newUser = new User();
-      newUser.setFirstName(firstName);
-      newUser.setLastName(lastName);
-      newUser.setEmail(email);
-      newUser.setPassword(passwordEncoder.encode(defaultPassword));
-      newUser.setRole(role);
+            return userRepository.save(newUser);
+        });
 
-      return userRepository.save(newUser);
-    });
+        var jwtToken = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
 
+        tokenService.revokeAllUserTokens(user);
+        tokenService.saveUserToken(user, jwtToken);
 
-    var jwtToken = jwtService.generateToken(user);
-    var refreshToken = jwtService.generateRefreshToken(user);
-
-    revokeAllUserTokens(user);
-
-
-    var token = new Token();
-    token.setUser(user);
-    token.setToken(jwtToken);
-    token.setTokenType(TokenType.BEARER);
-    token.setExpired(false);
-    token.setRevoked(false);
-    tokenRepository.save(token);
-
-    AuthenticationResponseDTO authResponse = new AuthenticationResponseDTO(jwtToken, refreshToken);
-
-    response.setContentType("application/json");
-    String jsonResponse = jacksonObjectMapper.writeValueAsString(authResponse);
-    response.getWriter().write(jsonResponse);
-    response.getWriter().flush();
-
-  }
-
-  private void revokeAllUserTokens(User user) {
-    var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
-    if (validUserTokens.isEmpty())
-      return;
-    validUserTokens.forEach(token -> {
-      token.setExpired(true);
-      token.setRevoked(true);
-    });
-    tokenRepository.saveAll(validUserTokens);
-  }
+        AuthenticationResponseDTO authResponse = new AuthenticationResponseDTO(jwtToken, refreshToken);
+        response.setContentType("application/json");
+        String jsonResponse = jacksonObjectMapper.writeValueAsString(authResponse);
+        response.getWriter().write(jsonResponse);
+        response.getWriter().flush();
+    }
 }
--- modules/shorturl-common/src/main/java/dev/shorturl/security/service/AuthService.java
+++ modules/shorturl-common/src/main/java/dev/shorturl/security/service/AuthService.java
@@ -3,10 +3,7 @@ package dev.shorturl.security.service;
 import dev.shorturl.security.dto.AuthenticationRequestDTO;
 import dev.shorturl.security.dto.AuthenticationResponseDTO;
 import dev.shorturl.security.dto.RegisterRequestDTO;
-import dev.shorturl.security.model.Token;
-import dev.shorturl.security.model.TokenType;
 import dev.shorturl.security.model.User;
-import dev.shorturl.security.repository.TokenRepository;
 import dev.shorturl.security.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,17 +16,16 @@ public class AuthService {
 
   private final UserRepository userRepository;
   private final JWTService jwtService;
-  private final TokenRepository tokenRepository;
   private final AuthenticationManager authenticationManager;
   private final PasswordEncoder passwordEncoder;
+  private final TokenService tokenService;
 
-
-  public AuthService(UserRepository userRepository, JWTService jwtService, TokenRepository tokenRepository, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
+  public AuthService(UserRepository userRepository, JWTService jwtService, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder, TokenService tokenService) {
     this.userRepository = userRepository;
     this.jwtService = jwtService;
-    this.tokenRepository = tokenRepository;
     this.authenticationManager = authenticationManager;
     this.passwordEncoder = passwordEncoder;
+    this.tokenService = tokenService;
   }
 
   public AuthenticationResponseDTO register(RegisterRequestDTO registerRequestDTO) {
@@ -41,23 +37,12 @@ public class AuthService {
     user.setRole(registerRequestDTO.role());
 
     var savedUser = userRepository.save(user);
-    var jwtToken = jwtService.generateToken(user);
-    var refreshToken = jwtService.generateRefreshToken(user);
+    var jwtToken = jwtService.generateToken(savedUser);
+    var refreshToken = jwtService.generateRefreshToken(savedUser);
 
-    saveUserToken(savedUser, jwtToken);
+    tokenService.saveUserToken(savedUser, jwtToken);
 
     return new AuthenticationResponseDTO(jwtToken, refreshToken);
-  }
-
-  private void saveUserToken(User user, String jwtToken) {
-    var token = new Token();
-    token.setUser(user);
-    token.setToken(jwtToken);
-    token.setTokenType(TokenType.BEARER);
-    token.setExpired(false);
-    token.setRevoked(false);
-
-    tokenRepository.save(token);
   }
 
   public AuthenticationResponseDTO authenticate(AuthenticationRequestDTO authenticationRequestDTO) {
@@ -72,20 +57,9 @@ public class AuthService {
 
     var jwtToken = jwtService.generateToken(user);
     var refreshToken = jwtService.generateRefreshToken(user);
-    revokeAllUserTokens(user);
-    saveUserToken(user, jwtToken);
+    tokenService.revokeAllUserTokens(user);
+    tokenService.saveUserToken(user, jwtToken);
 
     return new AuthenticationResponseDTO(jwtToken, refreshToken);
-  }
-
-  private void revokeAllUserTokens(User user) {
-    var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
-    if (validUserTokens.isEmpty())
-      return;
-    validUserTokens.forEach(token -> {
-      token.setExpired(true);
-      token.setRevoked(true);
-    });
-    tokenRepository.saveAll(validUserTokens);
   }
 }
