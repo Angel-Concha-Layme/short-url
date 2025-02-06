cbb50b5de9844c5e86a7a57e05f21cfb216a21b1
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-02-06 15:36:27 -0500

    feat(token): extract token management logic into TokenService
    - Move token saving and revocation functionality from AuthService and CustomOAuth2LoginSuccessHandler to a new TokenService.
    - This centralizes token-related logic and prepares for reuse.

---
+++ modules/shorturl-common/src/main/java/dev/shorturl/security/service/TokenService.java
@@ -0,0 +1,40 @@
+package dev.shorturl.security.service;
+
+import dev.shorturl.security.model.Token;
+import dev.shorturl.security.model.TokenType;
+import dev.shorturl.security.model.User;
+import dev.shorturl.security.repository.TokenRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+  private final TokenRepository tokenRepository;
+
+
+  public TokenService(TokenRepository tokenRepository) {
+    this.tokenRepository = tokenRepository;
+  }
+
+  public void revokeAllUserTokens(User user) {
+    var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+    if (validUserTokens.isEmpty()) {
+      return;
+    }
+    validUserTokens.forEach((token -> {
+      token.setExpired(true);
+      token.setRevoked(true);
+    }));
+    tokenRepository.saveAll(validUserTokens);
+  }
+
+  public void saveUserToken(User user, String jwtToken) {
+        var token = new Token();
+        token.setUser(user);
+        token.setToken(jwtToken);
+        token.setTokenType(TokenType.BEARER);
+        token.setExpired(false);
+        token.setRevoked(false);
+        tokenRepository.save(token);
+    }
+}
