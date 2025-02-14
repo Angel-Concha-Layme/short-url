bc6bd78c3bfb5064eddc2d40a32b3b800557f2e2
GuDex-Dev <108111815+GuDex-Dev@users.noreply.github.com>
2025-02-14 06:15:49 -0500

    feat(frontend): v1 changeTheme

--- modules/frontend/src/pages/index.tsx
+++ modules/frontend/src/pages/index.tsx
@@ -1,28 +1,19 @@
-import { useColorScheme, Switch } from "@mui/material";
-import Head from "next/head";
+import ChangeThemeSwitch from '@/components/changeTheme';
+import Head from 'next/head';
 
 export default function Home() {
-    const { mode, setMode } = useColorScheme();
+  return (
+    <>
+      <Head>
+        <title>Short Url</title>
+        <meta name="description" content="Short Url" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    if (!mode) {
-        return null;
-    }
-
-    return (
-        <>
-            <Head>
-                <title>Short Url</title>
-                <meta name="description" content="Short Url" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main>
-                <Switch
-                    checked={mode === "dark"}
-                    onChange={() => setMode(mode === "dark" ? "light" : "dark")}
-                />
-            </main>
-        </>
-    );
+      <main>
+        <ChangeThemeSwitch />
+      </main>
+    </>
+  );
 }
