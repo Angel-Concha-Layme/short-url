7f9f9e9c9fad99f0e1fd73c1cc7772b3f4575e56
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-02-14 23:20:43 -0500

    feat(model): add AppUser entity

---
+++ modules/shorturl-api/src/main/java/dev/shorturl/model/AppUser.java
@@ -0,0 +1,29 @@
+package dev.shorturl.model;
+
+import dev.shorturl.security.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "app_user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private User securityUser;
+
+  @OneToMany(mappedBy = "appUser")
+  private List<Link> links;
+}
