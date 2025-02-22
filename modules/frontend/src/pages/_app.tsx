bb0dc456cac7a5bc7e3e98c36356347546fbdae4
GuDex-Dev <108111815+GuDex-Dev@users.noreply.github.com>
2025-02-22 15:36:34 -0500

    chore(frontend): add roboto fonts

--- modules/frontend/src/pages/_app.tsx
+++ modules/frontend/src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { CssBaseline } from "@mui/material";
 
 const theme = createTheme({
