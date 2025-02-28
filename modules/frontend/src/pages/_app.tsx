09d5aba52a2f947cc110d92df6c35da51a2951ea
GuDex-Dev <108111815+GuDex-Dev@users.noreply.github.com>
2025-02-28 18:21:26 -0500

    SU-12: format _app.tsx

--- modules/frontend/src/pages/_app.tsx
+++ modules/frontend/src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { CssBaseline } from "@mui/material";
 
 const theme = createTheme({
   colorSchemes: {
     dark: true,
-  }
+  },
 });
 
 export default function App({ Component, pageProps }: AppProps) {
