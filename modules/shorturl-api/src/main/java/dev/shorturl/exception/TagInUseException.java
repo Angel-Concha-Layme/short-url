4ebe33eed8505aef04374288303b1cb18f006ace
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-03-13 08:20:21 -0500

    feat(exception): add TagInUseException to handle tags in use

---
+++ modules/shorturl-api/src/main/java/dev/shorturl/exception/TagInUseException.java
@@ -0,0 +1,7 @@
+package dev.shorturl.exception;
+
+public class TagInUseException extends ResourceNotFoundException {
+    public TagInUseException(Long tagId) {
+        super("Tag with ID " + tagId + " cannot be deleted because it is associated with one or more links");
+    }
+}
