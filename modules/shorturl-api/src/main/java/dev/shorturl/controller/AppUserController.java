d01c032adf99622e72b29740e8c5f295a4675b4e
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-03-13 10:10:18 -0500

    feat(app-user): add password change for users

--- modules/shorturl-api/src/main/java/dev/shorturl/controller/AppUserController.java
+++ modules/shorturl-api/src/main/java/dev/shorturl/controller/AppUserController.java
@@ -1,13 +1,29 @@
 package dev.shorturl.controller;
 
+import dev.shorturl.security.SecurityContext;
+import dev.shorturl.security.dto.ChangePasswordRequestDTO;
 import dev.shorturl.services.AppUserService;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/app-user")
 public class AppUserController {
   private final AppUserService appUserService;
 
   public AppUserController(AppUserService appUserService) {
     this.appUserService = appUserService;
   }
+
+  @PostMapping
+  @RequestMapping("/change-password")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  public ResponseEntity<Boolean> changePassword(@RequestBody ChangePasswordRequestDTO changePasswordRequestDTO) {
+    return ResponseEntity.ok(appUserService.tryChangePassword(changePasswordRequestDTO, SecurityContext.getUserOrFail()));
+  }
+
 }
--- modules/shorturl-api/src/main/java/dev/shorturl/services/AppUserService.java
+++ modules/shorturl-api/src/main/java/dev/shorturl/services/AppUserService.java
@@ -1,7 +1,10 @@
 package dev.shorturl.services;
 
+import dev.shorturl.model.AppUser;
 import dev.shorturl.repository.AppUserRepository;
+import dev.shorturl.security.dto.ChangePasswordRequestDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AppUserService {
@@ -9,5 +12,15 @@ public class AppUserService {
 
   public AppUserService(AppUserRepository appUserRepository) {
     this.appUserRepository = appUserRepository;
+  }
+
+  @Transactional
+  public Boolean tryChangePassword(ChangePasswordRequestDTO changePasswordRequestDTO, AppUser user) {
+    if (!user.getSecurityUser().getPassword().equals(changePasswordRequestDTO.oldPassword())) {
+      return false;
+    }
+    user.getSecurityUser().setPassword(changePasswordRequestDTO.newPassword());
+    appUserRepository.save(user);
+    return true;
   }
 }
