fa0b35d7e2ae513db0abd40aa25682e2b2785230
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-03-13 08:30:07 -0500

    feat(repository): add method to retrieve tags by user

--- modules/shorturl-api/src/main/java/dev/shorturl/repository/TagRepository.java
+++ modules/shorturl-api/src/main/java/dev/shorturl/repository/TagRepository.java
@@ -1,9 +1,14 @@
 package dev.shorturl.repository;
 
+import dev.shorturl.model.AppUser;
 import dev.shorturl.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findByAppUser(AppUser appUser);
 }
