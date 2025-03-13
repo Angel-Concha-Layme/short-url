bba1f615096c35a72ab1bd992a5bf5aadf38c55f
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-03-13 08:25:23 -0500

    refactor(model): add Builder pattern to Link entity

--- modules/shorturl-api/src/main/java/dev/shorturl/model/Link.java
+++ modules/shorturl-api/src/main/java/dev/shorturl/model/Link.java
@@ -1,10 +1,7 @@
 package dev.shorturl.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.Set;
@@ -15,6 +12,7 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Link {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
