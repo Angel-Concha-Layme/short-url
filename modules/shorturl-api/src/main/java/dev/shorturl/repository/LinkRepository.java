1d6af81da56619d7dc883675119f88647847a3a9
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-03-13 08:27:32 -0500

    feat(repository): add method to retrieve links by user

--- modules/shorturl-api/src/main/java/dev/shorturl/repository/LinkRepository.java
+++ modules/shorturl-api/src/main/java/dev/shorturl/repository/LinkRepository.java
@@ -1,9 +1,13 @@
 package dev.shorturl.repository;
 
+import dev.shorturl.model.AppUser;
 import dev.shorturl.model.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LinkRepository extends JpaRepository<Link, Long> {
+  List<Link> findByAppUser(AppUser user);
 }
