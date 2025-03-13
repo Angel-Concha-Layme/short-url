b083208077384bb1c261a47f0102a9c148d9e505
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-03-13 08:21:57 -0500

    feat(repository): add method to find AppUser by User in AppUserRepository

--- modules/shorturl-api/src/main/java/dev/shorturl/repository/AppUserRepository.java
+++ modules/shorturl-api/src/main/java/dev/shorturl/repository/AppUserRepository.java
@@ -1,8 +1,12 @@
 package dev.shorturl.repository;
 
 import dev.shorturl.model.AppUser;
+import dev.shorturl.security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
+    Optional<AppUser> findBySecurityUser(User securityUser);
 }
