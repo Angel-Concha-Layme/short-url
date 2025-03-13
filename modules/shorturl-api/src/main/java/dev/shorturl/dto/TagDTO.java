890d48dbcaf31209bc6102b10664c7b7385e1066
Angel Tomas Concha Layme <aconchal@unsa.edu.pe>
2025-03-13 08:29:43 -0500

    refactor(dto): change TagDTO to a record and update attributes

--- modules/shorturl-api/src/main/java/dev/shorturl/dto/TagDTO.java
+++ modules/shorturl-api/src/main/java/dev/shorturl/dto/TagDTO.java
@@ -1,15 +1,17 @@
 package dev.shorturl.dto;
 
+import dev.shorturl.model.Tag;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
-public class TagDTO {
-    private Long id;
-    
+public record TagDTO(
     @NotBlank(message = "Tag name cannot be empty")
-    private String name;
-    
-    @NotBlank(message = "Tag description cannot be empty")
-    private String description;
+    String name,
+
+    @NotBlank(message = "Tag color cannot be empty")
+    String color
+) {
+
+    public static TagDTO of(Tag tag) {
+        return new TagDTO(tag.getName(), tag.getColor());
+    }
 }
